75aee370cf11035471ba09ab664195d8f5a1f6df
Kate Opager <opagerk@byu.edu>
2023-01-31 12:18:51 -0700

    .

--- ASPNETWebApp/wwwroot/js/GradeCalculator.js
+++ ASPNETWebApp/wwwroot/js/GradeCalculator.js
@@ -1,15 +1,16 @@
-﻿function gradeCalculator(){
+﻿$("calculate").click(function () {
+    alert("hello");
     //Assign the inputs to the variables and make sure they are inputted as integer values rounded
-    let Assignments = (parseFloat(document.querySelector("#Assignments").Value)) * 50; //after parsing the integer, I multiply it by the weight value
-    let GroupProject = (parse.Float(document.querySelector("#GP").Value)) * 10;
-    let Quizzes = (parse.Float(document.querySelector("#Q").Value)) * 10;
-    let MidtermExam = (parse.Float(document.querySelector("#ME").Value)) * 10;
-    let FinalExam = (parse.Float(document.querySelector("#FE").Value)) * 10;
-    let Intex = (parse.Float(document.querySelector("#I").Value)) * 10;
+    let Assignments = (parseFloat($("#Assignments").val()) * 50); //after parsing the integer, I multiply it by the weight value
+    let GroupProject = (parse.Float($("#GP").val()) * 10);
+    let Quizzes = (parse.Float($("#Q").val()) * 10);
+    let MidtermExam = (parse.Float($("#ME").val()) * 10);
+    let FinalExam = (parse.Float($("#FE").val()) * 10);
+    let Intex = (parse.Float($("#I").val()) * 10);
 
-    let assignedGrade = "" //declared the variable so that it could be assigned a letter grade later
+    let assignedGrade = ""; //declared the variable so that it could be assigned a letter grade later
 
-    let finalGrade = (Assignments + GroupProject + Quizzes + MidtermExam + FinalExam + Intex) / 100 //tried to calculate overall grade, not sure if this is correct
+    let finalGrade = (Assignments + GroupProject + Quizzes + MidtermExam + FinalExam + Intex) / 100 //tried to calculate overall weighted grade, not sure if this is correct
     //used an if statement to go through all the possibilities for different grades
     if (finalGrade <= 100 && finalGrade >= 94) {
         assignedGrade = "A"
@@ -37,6 +38,7 @@
         assignedGrade = "E"
     }
 
+/*    after running through the if statement above, the assigned grade value would be displayed below back on the index page*/
 
-    document.querySelector("#result").innerHTML = ("Grade received will be ${ assignedGrade } with a ${finalGrade}%.")
-}
+    $("#result").text = ("Grade received will be ${ assignedGrade } with a ${finalGrade}%.");
+});
